7152deb97a5920420ea4eaeb1ce8bee8d724b9da
Shruti Rajani <45157836+shrutirajani@users.noreply.github.com>
2019-01-28 23:13:56 -0600

    Update Nested Lists.py

--- Nested Lists.py
+++ Nested Lists.py
@@ -18,3 +18,10 @@ for _ in range(0,int(input())):
 
 second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
 print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+
+
+
+##CONCLUSION##
+#There are  students in this class whose names and grades are assembled to build the following list:
+#python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+#The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
