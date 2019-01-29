e06ca1a728537ca349bd9473179325fa1425dbb4
Shruti Rajani <45157836+shrutirajani@users.noreply.github.com>
2019-01-28 23:12:05 -0600

    Update Nested Lists.py

--- Nested Lists.py
+++ Nested Lists.py
@@ -1,3 +1,17 @@
+#Given the names and grades for each student in a Physics class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+#Note: If there are multiple students with the same grade, order their names alphabetically and print each name on a new line.
+
+#Input Format
+
+#The first line contains an integer, , the number of students. 
+#The  subsequent lines describe each student over  lines; the first line contains a student's name, and the second line contains their grade.
+
+#Constraints
+
+#There will always be one or more students having the second lowest grade.
+
+
 marksheet = []
 for _ in range(0,int(input())):
     marksheet.append([input(), float(input())])
